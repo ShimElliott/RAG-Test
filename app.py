66921e9ca81d3c30da3803758257c5b73917316f
ShimElliott <shime@oregonstate.edu>
2024-11-01 09:45:06 -0700

    Modified version using langgraph that is capable of rememebering previous messages

--- app.py
+++ app.py
@@ -8,18 +8,28 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 
 os.environ["OPENAI_API_KEY"] = openai_api_key
 
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 llm = ChatOpenAI(model="gpt-4o-mini")
 
+from typing import Sequence
+
 import bs4
-from langchain import hub
-from langchain_chroma import Chroma
+
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.messages import AIMessage, BaseMessage, HumanMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
-from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, StateGraph
+from langgraph.graph.message import add_messages
+from typing_extensions import Annotated, TypedDict
+from langchain import hub
 
 # Gets data from the pdf file.
 from langchain_community.document_loaders import PyPDFLoader
@@ -28,44 +38,93 @@ loader = PyPDFLoader("data.pdf")
 docs = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=200, add_start_index=True
+    chunk_size=1000, chunk_overlap=200
 )
 splits = text_splitter.split_documents(docs)
 
-vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
-
-# Retrieve and generate using data from the pdf file.
-retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
-
-# Custom template for RAG.
-from langchain_core.prompts import PromptTemplate
-template = """Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
-Be as concise as possible. You are a psychology professor and you are speaking to a student.
-You are by-the-book and always prefer the material in the context over your own knowledge.
-
-{context}
-
-Question: {question}
-
-
-Helpful Answer:
-"""
-
-prompt = PromptTemplate.from_template(template)
-
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
-
-
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
+vectorstore = InMemoryVectorStore.from_documents(
+    documents=splits,
+    embedding=OpenAIEmbeddings()
 )
 
-for chunk in rag_chain.stream("What are reflexes?"):
-    print(chunk, end="", flush=True)
+# Retrieve and generate using data from the pdf file.
+retriever = vectorstore.as_retriever()
 
-vectorstore.delete_collection()
+# Contextualize Question
+contextualize_q_system_prompt = (
+    "Given a chat history and the latest user question "
+    "which might reference context in the chat history, "
+    "formulate a standalone question which can be understood "
+    "without the chat history. Do NOT answer the question, "
+    "just reformulate it if needed and otherwise return it as is."
+)
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
+)
+
+system_prompt = (
+    "You are a psychology professor and you are speaking to a student."
+    "You are by-the-book and always prefer the material in the context over your own knowledge."
+    "You use the following pieces of context to answer the question"
+    "If you don't know the answer, just say that you don't know, don't try to make up an answer."
+    "Be as concise as possible."
+    "\n\n"
+    "{context}"
+)
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+# Manages chat history
+class State(TypedDict):
+    input: str
+    chat_history: Annotated[Sequence[BaseMessage], add_messages]
+    context: str
+    answer: str
+
+def call_model(state: State):
+    response = rag_chain.invoke(state)
+    return {
+        "chat_history": [
+            HumanMessage(state["input"]),
+            AIMessage(response["answer"]),
+        ],
+        "context": response["context"],
+        "answer": response["answer"],
+    }
+
+
+workflow = StateGraph(state_schema=State)
+workflow.add_edge(START, "model")
+workflow.add_node("model", call_model)
+
+memory = MemorySaver()
+app = workflow.compile(checkpointer=memory)
+
+config = {"configurable": {"thread_id": "user1"}}
+
+result = app.invoke(
+    {"input": "What was Pavlov?"},
+    config=config,
+)
+print(result["answer"])
+
+result = app.invoke(
+    {"input": "What was he known for?"},
+    config=config,
+)
+print(result["answer"])
